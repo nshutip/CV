cba5fd3c93662917e859224bbf2dcc82e73fef40
Nshuti Parfait <nshutiparfait99@gmail.com>
2022-12-20 09:06:02 +0300

    new changes

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-window.onscroll = function() {myFunction()};
-
-
-var header = document.getElementById("myHeader");
-
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("header");
-  } else {
-    header.classList.remove("header");
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
 }
+
+function setRandomColor(div) {
+  div.css("color", getRandomColor());
+}
+
+// document.getElementsById('main-title')[0]
+//   .addEventListener('click', function setRandomColor(event) {
+//     event.css("color", getRandomColor());
+//   });
+
+// const mainTitle = document.getElementById('main-title');
+
+// if (mainTitle) {
+//   mainTitle.addEventListener('click', function (event){
+//     event.css("color", getRandomColor);
+//     console.log(getRandomColor);
+//   })
+// };
